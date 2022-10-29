779b75cd0ec6d9dc4f5dbbc68e483a7e966dd843
Igor <zamuragin03@gmail.com>
2022-10-30 01:15:38 +0300

    added new workind with db. fix needed

---
+++ test/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace test
+{
+    class Program
+    {
+        static FileTaskEditor fl;
+        static void Main(string[] args)
+        {
+            fl = new();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(fl.GetTask(i).Task1);
+            }
+        }
+    }
+    class FileTaskEditor
+    {
+        private readonly string path = @"C:\Users\zamur\OneDrive\Рабочий стол\planner\tasks.csv";
+        private List<Task> tasks;
+        private CsvConfiguration config;
+        public FileTaskEditor()
+        {
+            config = new CsvConfiguration(CultureInfo.CurrentCulture) { Delimiter = ";", Encoding = Encoding.UTF8 };
+        }
+
+        public int GetLength(int row)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                using (var csvreader = new CsvReader(reader, config))
+                {
+                    csvreader.Context.Configuration.DetectDelimiter = true;
+                    tasks = csvreader.GetRecords<Task>().ToList();
+                }
+            }
+            switch (row)
+            {
+                case 1:
+                    return tasks[0].Task1.Length;
+                case 2:
+                    return tasks[0].Task2.Length;
+                case 3:
+                    return tasks[0].Task3.Length;
+                case 4:
+                    return tasks[0].Task4.Length;
+            }
+            return 0;
+        }
+
+
+        public Task GetTask(int i)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                using (var csvreader = new CsvReader(reader, config))
+                {
+                    csvreader.Context.Configuration.DetectDelimiter = true;
+                    tasks = csvreader.GetRecords<Task>().ToList();
+                }
+            }
+
+            return tasks[i];
+        }
+        public void AddRow(Task task)
+        {
+            tasks = new List<Task>()
+            {
+                task
+            };
+            var newconfig = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                HasHeaderRecord = false,
+                Delimiter = ";",
+                Encoding = Encoding.UTF8
+            };
+            using (var stream = File.Open(path, FileMode.Append))
+            using (var writer = new StreamWriter(stream))
+            using (var csv = new CsvWriter(writer, newconfig))
+            {
+                csv.Context.RegisterClassMap<TaskMap>();
+                csv.WriteRecords(tasks);
+            }
+        }
+
+    }
+
+    public class Task
+    {
+        [Name("Task1")]
+        public string? Task1 { get; set; }
+        [Name("Task2")]
+        public string? Task2 { get; set; }
+        [Name("Task3")]
+        public string? Task3 { get; set; }
+        [Name("Task4")]
+        public string? Task4 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Task1} {Task2} {Task3} {Task4}\n";
+        }
+    }
+    public class TaskMap : ClassMap<Task>
+    {
+        public TaskMap()
+        {
+            Map(x => x.Task1).Name("Task1");
+            Map(x => x.Task2).Name("Task2");
+            Map(x => x.Task3).Name("Task3");
+            Map(x => x.Task4).Name("Task4");
+        }
+    }
+}
--- todayplanner/MainWindow.xaml.cs
+++ todayplanner/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,49 +11,139 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace todayplanner
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+
+
+    delegate void OnChanged(string type);
+
     public partial class MainWindow : Window
     {
+        SQLiteConnection db;
+        private string content;
+        private OnChanged onChangedHandler;
         public MainWindow()
         {
             InitializeComponent();
+            db = new SQLiteConnection("Data Source = tasks.db;");
+            db.Open();
+            onChangedHandler = Update;
+            Update("Срочно и важно");
+            Update("Не срочно и важно");
+            Update("Срочно и не важно");
+            Update("Не срочно и не важно");
+
         }
 
-        private void Add(object sender, RoutedEventArgs e)
+        void Update(string type)
         {
-            if (TextBox1.Text!="")
+            switch (type)
             {
-                listbox1.Items?.Add(TextBox1?.Text);
+                case "Срочно и важно":
+                    UpdateListBox(ListBox1, type);
+                    break;
+                case "Не срочно и важно":
+                    UpdateListBox(ListBox2, type);
+                    break;
+                case "Срочно и не важно":
+                    UpdateListBox(ListBox3, type);
+                    break;
+                case "Не срочно и не важно":
+                    UpdateListBox(ListBox4, type);
+                    break;
             }
-
-            TextBox1.Text = null;
         }
 
-        private void Remove(object sender, RoutedEventArgs e)
+        void UpdateListBox(ListBox listbox, string type)
         {
-            try
+            SQLiteCommand command;
+            SQLiteDataReader reader;
+            command = new SQLiteCommand($"Select Text from data where column= '{type}'", db);
+            reader = command.ExecuteReader();
+            foreach (DbDataRecord el in reader)
             {
-                listbox1.Items.RemoveAt(listbox1.Items.IndexOf(listbox1?.SelectedItem));
+                listbox.Items.Add(el["Text"]);
             }
-            catch (InvalidOperationException)
-            {
+            command.Dispose();
+        }
+        //protected override void OnClosed(EventArgs e)
+        //{
+        //    MessageBoxResult result = MessageBox.Show("Сохранить данные?", "Planner", MessageBoxButton.YesNo);
+        //    switch (result)
+        //    {
+        //        case MessageBoxResult.Yes:
+        //            break;
+        //        case MessageBoxResult.No:
+        //            break;
+        //    }
+        //}
 
-            }
-            catch (ArgumentOutOfRangeException)
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextRange textRange = new TextRange(Content_box.Document.ContentStart, Content_box.Document.ContentEnd);
+            content = textRange.Text;
+            string type = message_type.Text;
+            Content_box.Document.Blocks.Clear();
+            switch (type)
             {
-
+                case "Срочно и важно":
+                    SetData("Срочно и важно");
+                    break;
+                case "Не срочно и важно":
+                    SetData("Не срочно и важно");
+                    break;
+                case "Срочно и не важно":
+                    SetData("Срочно и не важно");
+                    break;
+                case "Не срочно и не важно":
+                    SetData("Не срочно и не важно");
+                    break;
+                default:
+                    MessageBox.Show("Выберите важность");
+                    return;
             }
+
+            content = "";
+        }
+
+        void SetData(string str)
+        {
+            SQLiteCommand command;
+            command = new SQLiteCommand("insert into data values (@text,@column)", db);
+            command.Parameters.AddWithValue("@text", content);
+            command.Parameters.AddWithValue("@column", str);
+            command.ExecuteNonQuery();
+            onChangedHandler.Invoke(str);
+        }
+
+        private void ListBox1_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            string temp = ((ListBox)sender).SelectedItem.ToString();
+            MessageBox.Show(temp);
+
+        }
+
+        //private void ListBox1_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        //{
+        //    string temp = ((ListBox)sender).SelectedItem.ToString();
+        //    MessageBox.Show(temp);
+        //}
+
+        private void Delete_selected(object sender, RoutedEventArgs e)
+        {
+            SQLiteCommand command = new SQLiteCommand("delete from data", db);
+            command.ExecuteNonQuery();
+            command.Dispose();
+            Update("Срочно и важно");
+            Update("Не срочно и важно");
+            Update("Срочно и не важно");
+            Update("Не срочно и не важно");
 
         }
     }
+
 }
